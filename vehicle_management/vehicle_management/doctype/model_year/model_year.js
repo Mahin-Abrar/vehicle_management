7b2aa45f3ae0826def81fdacbca11d35b0506b68
Mahin-Abrar <mahinabrar25@gmail.com>
2023-12-14 17:05:37 +0600

    Until Task 5 and field clearation task

---
+++ vehicle_management/vehicle_management/doctype/model_year/model_year.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Mahin Abrar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Model Year', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ vehicle_management/vehicle_management/doctype/vehicle_availability/vehicle_availability.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, Mahin Abrar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Vehicle Availability', {
+		status: function(frm){
+                frm.set_value({'ship_details':'','in_house_address':'','port_location':'','shed_number':'','workshop_address':'','other':''});         
+		}
+});
+
