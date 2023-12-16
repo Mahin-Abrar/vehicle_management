35fa29c5ed6b7f35a284e23fa34b9ebc5b16cc3e
Mahin-Abrar <mahinabrar25@gmail.com>
2023-12-16 20:16:16 +0600

    appi inclusions

--- vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.js
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.js
@@ -1,25 +1,17 @@
 // Copyright (c) 2023, Mahin Abrar and contributors
 // For license information, please see license.txt
-
 frappe.ui.form.on('Vehicle Price', {
-	
-
-	company_price:function(frm){
-		
-	let companyPrice=frm.doc.company_price;
-	let customerPrice=frm.doc.customer_price;
-	let salePrice=companyPrice+customerPrice;
-	frm.set_value({'sale_price' :salePrice});	
-	},
-	customer_price:calculators(frm)
-	
+    company_price: function (frm) {
+        calculatePrices(frm);
+    },
+    customer_price: function (frm) {
+        calculatePrices(frm);
+    }
 });
-function calculators(frm){
-	let companyPrice=frm.doc.company_price;
-		let customerPrice=frm.doc.customer_price;
-		let salePrice=companyPrice+customerPrice;
-		frm.set_value({'sale_price':salePrice});	
+
+function calculatePrices(frm) {
+    let companyPrice = frm.doc.company_price || 0;
+    let customerPrice = frm.doc.customer_price || 0;
+    let salePrice = companyPrice + customerPrice;
+    frm.set_value('sale_price', salePrice);
 }
-
-
-
