7459d200382c23af68a094937a8df8a1f9419dbf
Mahin-Abrar <mahinabrar25@gmail.com>
2023-12-17 12:01:12 +0600

    code for the calculation

--- vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.js
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.js
@@ -8,10 +8,40 @@ frappe.ui.form.on('Vehicle Price', {
         calculatePrices(frm);
     }
 });
+//calculation for the chil table
+frappe.ui.form.on('Other Vehicle Items', {
+    quantity: function(frm, cdt, cdn) {
+        amount_child(frm, cdt, cdn);
+    },
+    rate: function(frm, cdt, cdn) {
+        amount_child(frm, cdt, cdn);
+    }
+});
 
+
+
+
+
+
+
+
+
+// calculation for the sale price
 function calculatePrices(frm) {
     let companyPrice = frm.doc.company_price || 0;
     let customerPrice = frm.doc.customer_price || 0;
     let salePrice = companyPrice + customerPrice;
     frm.set_value('sale_price', salePrice);
 }
+
+// calculation for the child tabel price calculation
+function amount_child(frm, cdt, cdn) {
+    let item = locals[cdt][cdn];
+    let qty = item.quantity || 0;
+    let rate = item.rate || 0;
+    let amount = qty * rate;
+
+    frappe.model.set_value(cdt, cdn, 'amount', amount);
+    frm.refresh_field('other_vehicle_items');
+}
+
