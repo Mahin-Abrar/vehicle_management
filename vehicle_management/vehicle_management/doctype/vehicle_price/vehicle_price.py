c10b6d384299519633a4a288d38fbd6d5ac27114
Mahin-Abrar <mahinabrar25@gmail.com>
2023-12-18 12:34:41 +0600

    backend calculation

--- vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2023, Mahin Abrar and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class VehiclePrice(Document):
-	pass
+    def validate(self):
+        self.sale_price = self.customer_price+self.company_price
+        self.grand_total=self.other_items_total +self.sale_price
+        self.other_items_total = 0
+        for row in self.get('other_vehicle_items'):
+            row.amount=row.quantity*row.rate
+            self.other_items_total += row.amount
