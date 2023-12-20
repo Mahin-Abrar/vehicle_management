59bab41bbbc440cd72f68bac7b3e5bb810a28c68
Mahin-Abrar <mahinabrar25@gmail.com>
2023-12-20 11:13:23 +0600

    Refactored code for delete_and_add field values to detals_and_availability

--- vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
@@ -3,18 +3,44 @@
 
 import frappe
 from frappe.model.document import Document
-
 from frappe import get_all, get_doc
+
+
 
 class VehiclePrice(Document):
     def on_submit(self):
-            doc = frappe.get_doc('Vehicle Details', self.vehicle_chassis_no)
-            doc.customer = self.customer
-            doc.is_sold=self.is_sold
-            p_key=frappe.db.get_list('Vehicle Availability',filters={
-                'vehicle_chassis_no':self.vehicle_chassis_no
-                },pluck='name')
+        self.add_vehicle_details()
+        self.add_vehicle_availability()
+    
+    def on_cancel(self):
+        self.rmv_vehicle_details()
+        self.rmv_vehicle_availability()
+    
+    def add_vehicle_details(self):
+         doc = frappe.get_doc('Vehicle Details', self.vehicle_chassis_no)
+         doc.customer = self.customer
+         doc.is_sold=self.is_sold
+         doc.save()
+         
+    def add_vehicle_availability(self):
+            p_key=frappe.db.get_list('Vehicle Availability',filters={'vehicle_chassis_no':self.vehicle_chassis_no},pluck='name')
+            doc2= frappe.get_doc('Vehicle Availability',p_key)
             doc2=frappe.get_doc('Vehicle Availability',p_key)
             doc2.customer = self.customer
+            doc2.is_sold = self.is_sold
             doc2.save()
+        
+    def rmv_vehicle_details(self):
+            doc = frappe.get_doc('Vehicle Details', self.vehicle_chassis_no)
+            doc.customer=''
+            doc.is_sold = False
             doc.save()
+        
+    def rmv_vehicle_availability(self):
+            p_key=frappe.db.get_list('Vehicle Availability',filters={
+                    'vehicle_chassis_no':self.vehicle_chassis_no
+                    },pluck='name')
+            doc2=frappe.get_doc('Vehicle Availability',p_key)
+            doc2.customer=''
+            doc2.is_sold=False
+            doc2.save()
