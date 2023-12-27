d338bb7fe8d36a7564ada9d896c4deb97d534679
Mahin-Abrar <mahinabrar25@gmail.com>
2023-12-27 10:26:04 +0600

    Code Refactored to standard

--- vehicle_management/vehicle_management/report/vehicle_test_2/vehicle_test_2.js
+++ vehicle_management/vehicle_management/report/vehicle_test_2/vehicle_test_2.js
@@ -32,16 +32,16 @@ frappe.query_reports["Vehicle Test 2"] = {
 				"options": "Country",
 				},
 				{
-				"fieldname": "from_date",
-				"label": "From Date",
-				"fieldtype": "Date",
-				// "options":"Vehicle Price"
+					fieldname: "from_date",
+					label: "Date From Filter",
+					fieldtype: "Date",
+					default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
 				},
 				{
-				"fieldname": "to_date",
-				"label": "To Date",
-				"fieldtype": "Date",
-				// "options":"Vehicle Price"
+					fieldname: "to_date",
+					label: "Date To Filter",
+					fieldtype: "Date",
+					default: frappe.datetime.get_today()
 				}
 				
 	]
--- vehicle_management/vehicle_management/report/vehicle_test_2/vehicle_test_2.py
+++ vehicle_management/vehicle_management/report/vehicle_test_2/vehicle_test_2.py
@@ -5,12 +5,50 @@ import frappe
 from frappe.query_builder import DocType
 from frappe import _
 
-def execute(filters=None):
+def execute(filters=None):  
+    if not filters: filters = {}
+    if filters.get('from_date') and filters.get('to_date') and filters.get('to_date') < filters.get('from_date'):
+        frappe.throw("The 'From Date' ({}) must be before the 'To Date' ({})".format(filters.from_date, filters.to_date))
     return get_columns(filters), get_data(filters)
 
 
+####using query builder #####
+def get_data(filters):
+    vehicle_details=frappe.qb.DocType("Vehicle Details")
+    vehicle_availability=frappe.qb.DocType("Vehicle Availability")
+    vehicle_price=frappe.qb.DocType("Vehicle Price")
+    query=(
+        frappe.qb.from_(vehicle_details)
+        .left_join(vehicle_availability)
+        .on(vehicle_details.chassis_number ==vehicle_availability.vehicle_chassis_no)
+        .left_join(vehicle_price)
+        .on(vehicle_details.chassis_number ==vehicle_price.vehicle_chassis_no)
+        .select(vehicle_details.chassis_number, vehicle_details.car_model, vehicle_details.model_year,
+            vehicle_details.shape, vehicle_details.auction_grade, vehicle_details.package,
+            vehicle_details.color, vehicle_details.milage, vehicle_details.cc,
+            vehicle_details.seat_capacity, vehicle_details.origin_country,
+            vehicle_availability.status, vehicle_price.sale_price,
+            vehicle_price.posting_date,
+            vehicle_price.grand_total, vehicle_price.customer) 
+     )
+    
+    if filters.get("chassis_number"):
+        query = query.where(vehicle_details.chassis_number ==filters.get("chassis_number"))
+    if filters.get("car_model"):
+        query = query.where(vehicle_details.car_model ==filters.get("car_model"))
+    if filters.get("shape"):
+        query = query.where(vehicle_details.shape ==filters.get("shape"))
+    if filters.get("origin_country"):
+        query = query.where(vehicle_details.origin_country ==filters.get("origin_country"))
+    if filters.get('from_date'):
+        query = query.where(vehicle_price.posting_date >= filters.get('from_date'))
+    if filters.get('to_date'):
+        query = query.where(vehicle_price.posting_date <= filters.get('to_date'))
+    
+    data=query.run(as_dict=True)
+    return data
 
-## for generating the columns     
+# for generating the columns     
 def get_columns(filters):
     return [
 		 {
@@ -35,77 +73,3 @@ def get_columns(filters):
         {"fieldname": "customer", "label": "Customer","fieldtype": "Data"},
         {"fieldname": "posting_date", "label": "Posting Date","fieldtype": "Date"}
 	]
-
-## without using query builder##################
-# def create_conditions(filters):
-#     VehicleDetails=frappe.qb.DocType("Vehicle Details")
-#     VehicleAvailability=frappe.qb.DocType("Vehicle Availability")
-#     VehiclePrice=frappe.qb.DocType("Vehicle Price")
-#     conditions = ""
-#     if filters.get('chassis_number'):
-#         conditions += "where VehicleDetails.chassis_number = %(chassis_number)s"
-        
-#     if filters.get('car_model'):
-#         conditions += "where VehicleDetails.car_model = %(car_model)s"
-        
-#     if filters.get('shape'):
-#         conditions += "where VehicleDetails.shape = %(shape)s"    
-    
-#     if filters.get('origin_country'):
-#         conditions += "where VehicleDetails.origin_country = %(origin_country)s"    
-      
-#     return conditions
-
-####using query builder #####
-def get_data(filters):
-    # conditions=create_conditions(filters)
-    VehicleDetails=frappe.qb.DocType("Vehicle Details")
-    VehicleAvailability=frappe.qb.DocType("Vehicle Availability")
-    VehiclePrice=frappe.qb.DocType("Vehicle Price")
-    query=(
-        frappe.qb.from_(VehicleDetails)
-        .left_join(VehicleAvailability)
-        .on(VehicleDetails.chassis_number ==VehicleAvailability.vehicle_chassis_no)
-        .left_join(VehiclePrice)
-        .on(VehicleDetails.chassis_number ==VehiclePrice.vehicle_chassis_no)
-        .select(VehicleDetails.chassis_number, VehicleDetails.car_model, VehicleDetails.model_year,
-            VehicleDetails.shape, VehicleDetails.auction_grade, VehicleDetails.package,
-            VehicleDetails.color, VehicleDetails.milage, VehicleDetails.cc,
-            VehicleDetails.seat_capacity, VehicleDetails.origin_country,
-            VehicleAvailability.status, VehiclePrice.sale_price,
-            VehiclePrice.posting_date,
-            VehiclePrice.grand_total, VehiclePrice.customer)
-       
-     )
-    if filters.get("chassis_number"):
-        query = query.where(VehicleDetails.chassis_number ==filters.get("chassis_number"))
-    if filters.get("car_model"):
-        query = query.where(VehicleDetails.car_model ==filters.get("car_model"))
-    if filters.get("shape"):
-        query = query.where(VehicleDetails.shape ==filters.get("shape"))
-    if filters.get("origin_country"):
-        query = query.where(VehicleDetails.origin_country ==filters.get("origin_country"))
-    if filters.get("from_date") and filters.get("to_date") and filters.get("to_date") <filters.get("from_date"):
-        frappe.throw("The 'From Date' ({0}) must be before the 'To Date' ({1})".format(filters.from_filter, filters.to_filter))
-    else:
-        # query=query.where(VehiclePrice.posting_date[filters.get("from_date"):filters.get("to_date")])
-        query=query.where(
-        (VehiclePrice.posting_date <= filters.get("from_date")) and (VehiclePrice.posting_date >= filters.get("to_date"))
-    )
-    
-    data=query.run(as_dict=True)
-    return data
-
-
-##for fetching the values for the column
-# def get_data(filters):
-#     conditions = create_conditions(filters)
-#     query = frappe.db.sql("""    select
-#             vd.chassis_number,vd.car_model,vd.model_year,vd.shape,vd.auction_grade,vd.package,vd.color,vd.milage,vd.cc,vd.seat_capacity,vd.origin_country,va.status,vp.sale_price,vp.grand_total,vp.customer
-#         FROM
-#             `tabVehicle Details` vd         
-#             LEFT JOIN
-#             `tabVehicle Availability` va ON vd.chassis_number = va.vehicle_chassis_no
-#             LEFT JOIN
-#             `tabVehicle Price` vp ON vd.chassis_number = vp.vehicle_chassis_no %s """ % (conditions), filters, as_list = 1)
-#     return query
